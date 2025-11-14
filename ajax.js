8b129f18d047426967cbb2515864148955e823f8
katchaucris95-eng <katchaucris95@gmail.com>
2025-11-14 19:50:23 +0000

    ajax

--- ajax.js
+++ ajax.js
@@ -9,12 +9,19 @@ const url = "https://jsonplaceholder.typicode.com/todos/1"; // endpoint
       // --- exemplo com fetch (promises / async) ---
       document
       .getElementById("btn-fetch")
-      .addEventListener("click", async () =>) {
+      .addEventListener("click", async () => {
         output.textContent = "";
         setLoading(true);
         try {
           // fetch retorna uma Promise que resolve para Response
           const res = await fetch(url, { method: "GET"});
-          if (!res)
+          if (!res.ok) throw new Error("Status HTTP: " + res.status);
+          const data = await res.json(); // parse JSON automaticamente
+          output.textContent = JSON.stringify(data, null, 2);
+        } catch (err) {
+          output.textContent = "Erro: " + err.message;
+          output.classList.add("error");
+        } finally {
+          setLoading(false);
         }
-      }
+      })
